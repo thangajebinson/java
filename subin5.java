f0180cd2cf3d972f1b14791f5d1d500df4d4f4b4
SUBIN03112005 <subinsr461@gmail.com>
2025-03-12 15:46:30 +0530

    hi

---
+++ subin5.java
@@ -0,0 +1,10 @@
+public class subin5 
+{
+    public static void main(String[] args) 
+    {
+        String firstName = "vishal";
+        String lastName = "kannan";
+        String fullName = firstName + lastName;
+        System.out.println("Full Name: " + fullName);
+    }
+}
