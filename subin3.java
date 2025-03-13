bbf5d78f650fb2269a9186c542583f2502dbf0fe
SUBIN03112005 <subinsr461@gmail.com>
2025-03-13 13:17:49 +0530

    world

---
+++ subin3.java
@@ -0,0 +1,16 @@
+public class subin3
+{
+    public static void main(String []args)
+    {
+        int a=10;
+        int b=20;
+        if(a>10 && b<20)
+        {
+            System.out.println("both conditions are true");
+        }
+        else
+        {
+            System.out.println("atleast one condition is true");
+        }
+    }
+}
