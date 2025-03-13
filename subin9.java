465a88b16b480a93db72344adfbbfbd9b9d0edb2
SUBIN03112005 <subinsr461@gmail.com>
2025-03-13 15:06:06 +0530

    major

---
+++ subin9.java
@@ -0,0 +1,15 @@
+public class subin9
+{
+    public static void main(String[] args) 
+    {
+        int Age=10;
+        if(Age>=18) 
+        {
+            System.out.println("You are a major.");
+        } 
+        else 
+        {
+            System.out.println("You are not a major.");
+        }
+    }
+}
