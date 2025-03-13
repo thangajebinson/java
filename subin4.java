4a0cbad8c7471a465eeeea9d7b3226257b36cea7
SUBIN03112005 <subinsr461@gmail.com>
2025-03-13 13:45:00 +0530

    play

---
+++ subin4.java
@@ -0,0 +1,11 @@
+public class subin4
+{
+    public static void main(String[] args) 
+    {
+       int a=3,b=4;
+       a=a+b;
+       b=a-b;
+       a=a-b;
+       System.out.println("a="+a+",b="+b);
+    }
+}
