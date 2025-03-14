764c9c3238564d5cad0d390a51b28299426624d9
SUBIN03112005 <subinsr461@gmail.com>
2025-03-14 13:35:49 +0530

    methods

---
+++ subin16.java
@@ -0,0 +1,11 @@
+public class subin16
+{
+    static void mymethod()
+    {
+       System.out.println("hello world");
+    }
+    public static void main(String[] args) 
+    {
+        mymethod();
+    }
+}
