b9918efbe943e080034f1112feb367933bbe7a14
SUBIN03112005 <subinsr461@gmail.com>
2025-03-14 10:42:32 +0530

    count

---
+++ subin12.java
@@ -0,0 +1,12 @@
+public class subin12
+{
+    public static void main(String[]args)
+    {
+        int count=0;
+        while(count<5)
+        {
+            System.out.println(10);
+            count++;
+        }
+    }
+}
