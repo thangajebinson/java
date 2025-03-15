3665d3b58ee0f4b2e61d052795611feb995d599b
SUBIN03112005 <subinsr461@gmail.com>
2025-03-15 10:20:51 +0530

    method

---
+++ subin21.java
@@ -0,0 +1,15 @@
+public class subin21
+{
+    public int id;
+    String student;
+}
+class TestStudent
+{
+    public static void main(String[] args) 
+    {
+       subin21 s1=new subin21();
+       s1.id=101;
+       System.out.println("s1.id");
+    }
+}
+
