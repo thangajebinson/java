819b6b5b8422e7f4a0a87074a7a3d32a29df4326
SUBIN03112005 <subinsr461@gmail.com>
2025-03-15 12:04:06 +0530

    scanner

---
+++ subin25.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class subin25
+{
+    public static void main(String[] args)
+    {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the name:");
+    String name=sc.nextLine();
+    System.out.println("name");
+    }
+}
