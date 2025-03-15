ca113a7a7eecea578024ba8f93e3a47f18b0720a
SUBIN03112005 <subinsr461@gmail.com>
2025-03-15 10:51:12 +0530

    invoice

---
+++ subin23.java
@@ -0,0 +1,11 @@
+public class subin23
+{
+   public static void main(String[] args) 
+   {
+      int [] a={1000,2000,3000,4000,5000};
+      for(int i=0;i<=a.length;i++)
+      {
+         System.out.println("the invoice generated numbers are:" +a[i]);
+      }
+   }
+}
