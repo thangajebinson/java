0c53a2d2750e285aaacb80998bb18905ac8036a0
SUBIN03112005 <subinsr461@gmail.com>
2025-03-15 11:13:25 +0530

    switch case

---
+++ subin24.java
@@ -0,0 +1,31 @@
+public class subin24
+{
+    public static void main(String[] args) 
+    {
+       int pick=5;
+       switch (pick) 
+       {
+           case 1:
+               System.out.println("customer choose the demo number:1");
+               break;
+            case 2:
+               System.out.println("customer choose the demo number:2");
+               break;
+            case 3:
+                 System.out.println("customer choose the demo number:3");
+                 break; 
+            case 4:
+                System.out.println("customer choose the demo number:4");
+               break;
+            case 5:
+               System.out.println("customer choose the demo number:5");
+               break;
+            case 6:
+                System.out.println("customer choose the demo number:6");
+               break;
+           default:
+               System.out.println ("the demo number is invalid");
+
+       }  
+    }
+}
